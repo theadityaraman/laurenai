a2dff545734c441a688a8c8f6a88441e15fa6864
Aditya Raman <114894627+theadityaraman@users.noreply.github.com>
2023-06-25 12:04:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,14 @@ function listen() {
     inputArea.innerHTML = event.results[0][0].transcript;
   }
 }
+function gpt(event) {
+  const { Configuration, OpenAIApi } = require("openai");
+  const configuration = new Configuration({
+    // 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 thats the key its name is LaurenAI2
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+const chat_completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: "Hello world" }],
+});
