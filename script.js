3f458273f33380fb6944578fefcca10dce81878d
Aditya Raman <114894627+theadityaraman@users.noreply.github.com>
2023-06-25 12:25:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+// Listening function setup
 function listen() {
   let inputArea = document.getElementById('input-area')
   let outputArea = document.getElementById('output-area')
-// speech rec config fuck shes playing me
+
+  // speech rec config fuck shes playing me
   var speech = new webkitSpeechRecognition();
   speech.lang = "en-US";
   speech.start();
-// config transcript i dont wanna go i thought we were going to the movies
+
+  // config transcript i dont wanna go i thought we were going to the movies
   speech.onresult = function(event) {
     let transcript = event.results[0][0].transcript;
     if (transcript.includes("hello")) {
@@ -18,14 +21,23 @@ function listen() {
     inputArea.innerHTML = event.results[0][0].transcript;
   }
 }
-function gpt(event) {
-  const { Configuration, OpenAIApi } = require("openai");
-  const configuration = new Configuration({
-    // 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 thats the key its name is LaurenAI2
-    apiKey: process.env.OPENAI_API_KEY,
+
+async function gpt(event) {
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
-const chat_completion = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: "Hello world" }],
+
+const response = await openai.createCompletion({
+  model: "text-davinci-003",
+  prompt: "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly."
+  // \n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: ",
+  temperature: 0.9,
+  max_tokens: 150,
+  top_p: 1,
+  frequency_penalty: 0,
+  presence_penalty: 0.6,
+  stop: [" Human:", " AI:"],
 });
